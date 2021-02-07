9916c677e1a83076a83ecd0e55dbd326ef9691fe
nazimudheen267 <nazimudheen267@gmail.com>
2021-02-07 02:20:42 -0500

    right column added in home section

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky"); 
-        }
         if(this.scroll > 500){
             $('scroll-up-btn').addClass("show");
         }else{
@@ -14,10 +9,15 @@ $(document).ready(function(){
 
     //slide-up
 
-    $('scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-    });
+   const toTop = document.querySelector(".to-top");
 
+   window.addEventListener("scroll", () => {
+        if (window.pageYoffset > 500){
+            topTop.classList.add("active");
+        }else{
+            toTop.classList.remove("active");
+        }
+   })
 
     //typing
 
@@ -29,7 +29,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Pentester", "Devoleper" , "Designer" , "Programmer" ,"youtuber" ],
+        strings: ["\n"+"Pentester","Devoleper" , "Designer" , "Programmer" ,"youtuber" ],
         typeSpeed:100,
         backSpeed:60,
         loop :true
